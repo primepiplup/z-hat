d092c92cd6a430c8b68d4e3ff48a02e7c5dbaa62
Daan Kuik <dkuik@sogyo.nl>
2024-01-29 15:52:21 +0100

    Added function to render ip address from integer

--- ip.zig
+++ ip.zig
@@ -29,8 +29,22 @@ pub fn networkPort(comptime port: u16) u16 {
     return std.mem.nativeToBig(u16, port);
 }
 
+pub fn ipStringFromAddr(ip_buffer: *[15]u8, addr: u32) []u8 {
+    var byte_buffer: [4]u8 = undefined;
+
+    std.mem.writeInt(u32, &byte_buffer, addr, std.builtin.Endian.little);
+
+    return std.fmt.bufPrint(ip_buffer[0..], "{}.{}.{}.{}", .{byte_buffer[0], byte_buffer[1], byte_buffer[2], byte_buffer[3]}) catch unreachable;
+}
+
 test "ip address is properly converted into an int" {
     try std.testing.expectEqual(ipAddr("127.0.0.1") catch unreachable, std.mem.nativeToBig(u32, 2130706433));
+}
+
+test "int is correctly converted into IP address" {
+    var ip_string_buffer: [15]u8 = undefined;
+    const ip_string = ipStringFromAddr(&ip_string_buffer, std.mem.nativeToBig(u32, 2130706433));
+    try std.testing.expect(std.mem.eql(u8, ip_string, "127.0.0.1"));
 }
 
 test "port is correctly put into network endianness" {
