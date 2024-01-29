3680be79eccc66872d463dc4c800fe650a513b6e
Daan Kuik <dkuik@sogyo.nl>
2024-01-29 17:06:11 +0100

    Client now polls both the stdin and the socket

--- client.zig
+++ client.zig
@@ -4,8 +4,12 @@ const config = @import("config.zig");
 
 const MAX_USERNAME_SIZE = config.MAX_USERNAME_SIZE;
 
-const stdin = std.io.getStdIn().reader();
+var fds: [2]std.os.pollfd = undefined;
+
+const stdin_file = std.io.getStdIn();
+const stdin = stdin_file.reader();
 const stdout = std.io.getStdOut().writer();
+
 
 pub fn main() !void {
     const socket = try std.os.socket(std.os.AF.INET, std.os.SOCK.STREAM, 0);
@@ -34,12 +38,23 @@ pub fn main() !void {
     _ = try std.os.connect(socket, @ptrCast(&sock_addr), 16);
     _ = try std.os.send(socket, username_buffer[0..username_len], 0);
 
+    fds[0].fd = socket;
+    fds[0].events = std.os.POLL.IN;
+    fds[1].fd = stdin_file.handle;
+    fds[1].events = std.os.POLL.IN | std.os.POLL.HUP;
+
     try stdout.print("Ready to send messages.\n", .{});
     while(!std.mem.eql(u8, &message, "/quit")) {
-        const message_len = getInput(&message) catch return;
-
-        _ = try std.os.send(socket, message[0..(message_len+1)], 0);
-        clearMessage(&message, message_len);
+        const event_count = try std.os.poll(&fds, 3000);
+        if(event_count <= 0) { continue; }
+        if(fds[1].revents == std.os.POLL.IN) {
+            const message_len = getInput(&message) catch return;
+            _ = try std.os.send(socket, message[0..(message_len+1)], 0);
+            clearMessage(&message, message_len);
+        } else if (fds[0].revents == std.os.POLL.IN) {
+            try stdout.print("there's a message that arrived from the server...", .{});
+            // There's a message from the server. Go and print it to the screen!
+        }
     }
 }
 
