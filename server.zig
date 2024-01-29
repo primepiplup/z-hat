d3edac35f7bdfd49b3f0289aff4da0786d883336
Daan Kuik <dkuik@sogyo.nl>
2024-01-29 08:41:56 +0100

    Server accepts connections on a streaming socket - single threaded still

--- server.zig
+++ server.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const ip = @import("ip.zig");
 
 pub fn main() !void {
-    const socket: std.os.socket_t = try std.os.socket(std.os.AF.INET, std.os.SOCK.DGRAM, 0);
+    const socket: std.os.socket_t = try std.os.socket(std.os.AF.INET, std.os.SOCK.STREAM, 0);
     const ip_addr = try ip.ipAddr("127.0.0.1");
 
     var pad: [8]u8 = undefined;
@@ -15,21 +15,29 @@ pub fn main() !void {
         .zero = pad,
     };
     try std.os.bind(socket, @ptrCast(&sock_addr), 16);
+    try std.os.listen(socket, 10);
 
     var buffer: [1000]u8 = undefined;
 
     const stdout = std.io.getStdOut().writer();
     try stdout.print("Bound socket.\n", .{});
 
-    while(std.os.read(socket, &buffer)) |len| {
+    var client_addr: std.os.linux.sockaddr = undefined;
+    var client_addr_size: u32 = 16;
+    const client = try std.os.accept(socket, &client_addr, &client_addr_size, 0);
+
+    try stdout.print("Accepted client connection.\n", .{});
+
+    while(std.os.read(client, &buffer)) |len| {
         if(len <= 0) {
-            try stdout.print("Encountered EOF", .{});
+            try stdout.print("Client closed connection.\n", .{});
             break;
         }
         try stdout.print("{s}", .{buffer});
     } else |_| {
-        try stdout.print("Encountered read error", .{});
+        try stdout.print("Encountered read error\n", .{});
     }
+
 
     std.os.close(socket);
 }
