6edd3f07f8b6da0574410638893835a13d20ff57
Daan Kuik <dkuik@sogyo.nl>
2024-01-29 10:51:33 +0100

    Refactored server code to be more readable

--- server.zig
+++ server.zig
@@ -1,32 +1,20 @@
 const std = @import("std");
 const ip = @import("ip.zig");
 
+const socket_t = std.os.socket_t;
+const sockaddr = std.os.linux.sockaddr;
+
+const stdout = std.io.getStdOut().writer();
+const MAX_CLIENTS = 10;
+const MAX_MESSAGE_SIZE = 1000;
+
 pub fn main() !void {
-    const socket: std.os.socket_t = try std.os.socket(std.os.AF.INET, std.os.SOCK.STREAM, 0);
-    const ip_addr = try ip.ipAddr("127.0.0.1");
+    const socket = try serverStartListening();
+    defer std.os.close(socket);
 
-    var pad: [8]u8 = undefined;
-    @memset(&pad, 0);
+    var buffer: [MAX_MESSAGE_SIZE]u8 = undefined;
 
-    const sock_addr = std.os.linux.sockaddr.in {
-        .family = std.os.linux.AF.INET,
-        .port = ip.networkPort(6789),
-        .addr = ip_addr,
-        .zero = pad,
-    };
-    try std.os.bind(socket, @ptrCast(&sock_addr), 16);
-    try std.os.listen(socket, 10);
-
-    var buffer: [1000]u8 = undefined;
-
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("Bound socket.\n", .{});
-
-    var client_addr: std.os.linux.sockaddr = undefined;
-    var client_addr_size: u32 = 16;
-    const client = try std.os.accept(socket, &client_addr, &client_addr_size, 0);
-
-    try stdout.print("Accepted client connection.\n", .{});
+    const client = try acceptConnection(socket);
 
     while(std.os.read(client, &buffer)) |len| {
         if(len <= 0) {
@@ -37,17 +25,36 @@ pub fn main() !void {
     } else |_| {
         try stdout.print("Encountered read error\n", .{});
     }
-
-
-    std.os.close(socket);
 }
 
-const SockAddrIn = struct {
-    Addr: InAddr,
-};
+fn serverStartListening() !socket_t {
+    const socket: socket_t = try std.os.socket(std.os.AF.INET, std.os.SOCK.STREAM, 0);
+    const ip_addr = try ip.ipAddr("127.0.0.1");
 
-const InAddr = struct {
-    ip_addr: u32,
-};
+    var pad: [8]u8 = undefined;
+    @memset(&pad, 0);
 
+    const sock_addr = sockaddr.in {
+        .family = std.os.linux.AF.INET,
+        .port = ip.networkPort(6789),
+        .addr = ip_addr,
+        .zero = pad,
+    };
+    try std.os.bind(socket, @ptrCast(&sock_addr), 16);
+    try stdout.print("Bound socket.\n", .{});
+
+    try std.os.listen(socket, 10);
+    try stdout.print("Listening for incoming connections..\n", .{});
+
+    return socket;
+}
+
+fn acceptConnection(socket: socket_t) !socket_t {
+    var client_addr: sockaddr = undefined;
+    var client_addr_size: u32 = 16;
+    const client = try std.os.accept(socket, &client_addr, &client_addr_size, 0);
+
+    try stdout.print("Accepted client connection.\n", .{});
+    return client;
+}
 
