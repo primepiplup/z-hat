7db4653ceba0f9090986bf021fe5a840d9951177
Daan Kuik <dkuik@sogyo.nl>
2024-01-30 10:24:26 +0100

    Added server ip and port to config

--- config.zig
+++ config.zig
@@ -2,3 +2,5 @@ pub const MAX_CLIENTS = 10;
 pub const MAX_MESSAGE_SIZE = 1000;
 pub const MAX_USERNAME_SIZE = 25;
 pub const USERNAME_MSG_OFFSET = 2;
+pub const SERVER_IP = "127.0.0.1";
+pub const SERVER_PORT = 6789;
