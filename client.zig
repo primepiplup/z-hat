9446e490ac441b16aa1567ef0d3f0fe4bef426a4
Daan Kuik <dkuik@sogyo.nl>
2024-01-30 16:47:29 +0100

    Basic curses interface added to program - WIP

--- client.zig
+++ client.zig
@@ -1,71 +1,125 @@
 const std = @import("std");
 const ip = @import("ip.zig");
 const config = @import("config.zig");
+const c = @cImport({ @cInclude("curses.h"); });
 
 const MAX_USERNAME_SIZE = config.MAX_USERNAME_SIZE;
 const MAX_MESSAGE_SIZE = config.MAX_MESSAGE_SIZE;
 const USERNAME_MSG_OFFSET = config.USERNAME_MSG_OFFSET;
 
-var fds: [2]std.os.pollfd = undefined;
+var fds: [1]std.os.pollfd = undefined;
 
 const stdin_file = std.io.getStdIn();
 const stdin = stdin_file.reader();
 const stdout = std.io.getStdOut().writer();
 
-var msg_buffer: [MAX_USERNAME_SIZE + USERNAME_MSG_OFFSET + MAX_MESSAGE_SIZE]u8 = undefined;
+var receive_buffer: [MAX_USERNAME_SIZE + USERNAME_MSG_OFFSET + MAX_MESSAGE_SIZE:0]u8 = undefined;
+var message_buffer: [MAX_MESSAGE_SIZE:0]u8 = undefined;
+var username_buffer: [MAX_USERNAME_SIZE]u8 = undefined;
+
+var stdwin:     ?*c.WINDOW = null;
+var msgwin:     ?*c.WINDOW = null;
+var inputwin:   ?*c.WINDOW = null;
+var height: c_int = 0;
+var width:  c_int = 0;
+
+const EXIT: [*:0]const u8 = "/quit";
 
 pub fn main() !void {
     const socket = try std.os.socket(std.os.AF.INET, std.os.SOCK.STREAM, 0);
-    defer std.os.close(socket);
 
+    fds[0].fd = socket;
+    fds[0].events = std.os.POLL.IN;
+
+    defer gracefulShutdown("User quit program.", 0);
+
+    @memset(&username_buffer, 0);
+    @memset(&message_buffer, 0);
+
+    try stdout.print("Provide a username, max 25 characters.\n", .{});
+    const username_len = getInput(username_buffer[0..]) catch return;
+
+    initScreen();
+
+    const sock_addr = try buildSockAddr();
+    _ = std.os.connect(socket, @ptrCast(&sock_addr), 16) catch { gracefulShutdown("No server could be found", 4); };
+    _ = std.os.send(socket, username_buffer[0..username_len], 0) catch { gracefulShutdown("Was unable to send username to server, quitting.", 2); };
+
+    _ = c.waddstr(msgwin, "Connected to server");
+    while(true) {
+        _ = c.wgetnstr(inputwin, &message_buffer, MAX_MESSAGE_SIZE - 1);
+        const msg: [*:0]const u8 = &message_buffer;
+
+        if(std.mem.orderZ(u8, msg, EXIT).compare(.eq)) { break; }
+        
+        _ = try std.os.send(socket, std.mem.span(msg), 0);
+        clearMessage(&message_buffer);
+
+        const event_count = try std.os.poll(&fds, 3000);
+
+        if(event_count <= 0) { continue; }
+
+        if (fds[0].revents == std.os.POLL.IN) {
+            try readReceived();
+            try displayReceived();
+        }
+    }
+}
+
+fn gracefulShutdown(reason: []const u8, statuscode: u8) void {
+    if(stdwin != null) {
+        _ = c.endwin();
+    }
+
+    std.debug.print("{s}\n", .{reason});
+
+    std.os.close(fds[0].fd);
+    
+    std.process.exit(statuscode);
+}
+
+fn initScreen() void {
+    stdwin = c.initscr();
+
+    height = c.LINES;
+    width  = c.COLS;
+
+    if(height < 5 or width < 10) {
+        gracefulShutdown("Terminal too small, quitting.", 3);
+    }
+    
+    msgwin = c.newwin(height - 4, width - 2, 2, 1);
+    inputwin = c.newwin(1, width - 2, height - 1, 1);
+    _ = c.refresh();
+}
+
+fn buildSockAddr() !std.os.sockaddr.in {
     const ip_addr = try ip.ipAddr(config.SERVER_IP);
 
     var pad: [8]u8 = undefined;
     @memset(&pad, 0);
 
-    const sock_addr = std.os.linux.sockaddr.in {
+    return std.os.linux.sockaddr.in {
         .family = std.os.linux.AF.INET,
         .port = ip.networkPort(config.SERVER_PORT),
         .addr = ip_addr,
         .zero = pad,
     };
-
-    var message: [1000]u8 = undefined;
-    var username_buffer: [MAX_USERNAME_SIZE]u8 = undefined;
-    @memset(&username_buffer, 0);
-    @memset(&message, 0);
-
-    try stdout.print("Provide a username, max 25 characters.\n", .{});
-    const username_len = getInput(username_buffer[0..]) catch return;
-
-    _ = try std.os.connect(socket, @ptrCast(&sock_addr), 16);
-    _ = try std.os.send(socket, username_buffer[0..username_len], 0);
-
-    fds[0].fd = socket;
-    fds[0].events = std.os.POLL.IN;
-    fds[1].fd = stdin_file.handle;
-    fds[1].events = std.os.POLL.IN | std.os.POLL.HUP;
-
-    try stdout.print("Ready to send messages.\n", .{});
-    while(!std.mem.eql(u8, &message, "/quit")) {
-        const event_count = try std.os.poll(&fds, 3000);
-        if(event_count <= 0) { continue; }
-        if(fds[1].revents == std.os.POLL.IN) {
-            const message_len = getInput(&message) catch return;
-            _ = try std.os.send(socket, message[0..(message_len+1)], 0);
-            clearMessage(&message, message_len);
-        } else if (fds[0].revents == std.os.POLL.IN) {
-            _ = try std.os.recv(fds[0].fd, &msg_buffer, 0);
-            try stdout.print("{s}", .{msg_buffer});
-            @memset(&msg_buffer, 0);
-        }
-    }
 }
 
-fn clearMessage(buffer: []u8, length: usize) void {
-    for(0..(length+1)) |i| {
-        buffer[i] = 0;
-    }
+fn readReceived() !void {
+    _ = try std.os.recv(fds[0].fd, &receive_buffer, 0);
+}
+
+fn displayReceived() !void {
+    const received: [*:0]const u8 = &receive_buffer;
+    _ = c.wprintw(msgwin, received);
+    @memset(&receive_buffer, 0);
+    _ = c.refresh();
+}
+
+fn clearMessage(buffer: []u8) void {
+    @memset(buffer, 0);
 }
 
 fn getInput(buffer: []u8) !usize {
