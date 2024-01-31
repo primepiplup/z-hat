047278eba2f471de4ac08ec2244455ec4493d5e6
Daan Kuik <dkuik@sogyo.nl>
2024-01-31 10:11:25 +0100

    Server properly sends broadcasts using 0 bytes to delimit usernames

--- server.zig
+++ server.zig
@@ -4,11 +4,11 @@ const config = @import("config.zig");
 
 const socket_t = std.os.socket_t;
 const sockaddr = std.os.linux.sockaddr;
-const POLL = std.os.POLL;
+const POLL     = std.os.POLL;
 
-const MAX_CLIENTS = config.MAX_CLIENTS;
-const MAX_MESSAGE_SIZE = config.MAX_MESSAGE_SIZE;
-const MAX_USERNAME_SIZE = config.MAX_USERNAME_SIZE;
+const MAX_CLIENTS         = config.MAX_CLIENTS;
+const MAX_MESSAGE_SIZE    = config.MAX_MESSAGE_SIZE;
+const MAX_USERNAME_SIZE   = config.MAX_USERNAME_SIZE;
 const USERNAME_MSG_OFFSET = config.USERNAME_MSG_OFFSET;
 
 const stdout = std.io.getStdOut().writer();
@@ -24,8 +24,8 @@ var discard: [16]u8 = undefined;
 var broadcast_buffer: [200]u8 = undefined;
 
 const Client = struct {
-    username: [MAX_USERNAME_SIZE]u8 = undefined,
-    connection: sockaddr.in = undefined,
+    username:   [MAX_USERNAME_SIZE:0]u8 = undefined,
+    connection: sockaddr.in           = undefined,
 };
 
 var clients: [MAX_CLIENTS]Client = undefined;
@@ -123,6 +123,8 @@ fn acceptConnection() !void {
     var ip_buffer: [15]u8 = undefined;
     const client_ip = ip.ipStringFromAddr(&ip_buffer, client.connection.addr);
     try stdout.print("Client IP address: {s}\n\n", .{client_ip});
+
+    try broadcast("server message --- user '{s}' connected", .{@as([*:0]const u8, &client.username)});
 }
 
 fn readSocket(client_number: usize) !void {
@@ -173,7 +175,8 @@ fn cleanConnection(cn_idx: usize) !void {
 
     try stdout.print("Cleaned lost connection to client {}, username: {s}\n", .{cn_idx, clients[cn_idx - 1].username});
 
-    try broadcast("User: '{s}' disconnected", .{clients[cn_idx - 1].username});
+    const c_username: [*:0]const u8 = &clients[cn_idx - 1].username;
+    try broadcast("server message --- user '{s}' disconnected", .{c_username});
     @memset(&clients[cn_idx - 1].username, 0);
 
     clients[cn_idx - 1] = clients[connection_count];
